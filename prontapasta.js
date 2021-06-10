b3f0a27a49e3aeef8bb6c937f13832381ded730a
JuanCruzBarras <juancruzbarras@hotmail.com>
2021-06-10 11:48:56 -0300

    subiendo el proyecto

---
+++ prontapasta.js
@@ -0,0 +1,14 @@
+function cambiarClase() {
+  let siteNav = document.getElementById("site-nav");
+  siteNav.classList.toggle("site-nav-open");
+  let menuOpen = document.getElementById("menu-toggle");
+  menuOpen.classList.toggle("menu-open");
+}
+
+var redirectBtns = document.querySelectorAll(".btnRedireccion");
+
+
+function abrir() {
+  document.getElementById("vent").style.display = "block";
+}
+
